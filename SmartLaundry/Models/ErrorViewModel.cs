8297475c2f887002b41526b6ffed568418b38069
Aleksander Ligęza <aleksligeza@gmail.com>
2018-03-22 21:31:44 +0100

    Add project files.

---
+++ SmartLaundry/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartLaundry.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
